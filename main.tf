8feea2d503995c5432783503eb089ce7689b0214
Nikita Pivkin <nikita.pivkin@smartforce.io>
2024-12-04 17:37:18 +0600

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,5 @@
-module "bucket" {
-	source = "./modules/s3"
+resource "aws_lambda_function" "test_lambda" {
+  function_name = "lambda_function_name"
+  role          = aws_iam_role.iam_for_lambda.arn
+  handler       = "index.test"
 }
